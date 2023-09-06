6985d802bcd2ea4b4613131004687e0a301b7170
broken99records <40528283+broken99records@users.noreply.github.com>
2023-09-06 16:22:30 -0400

    navigate to href value if it is not null, and take screenshot

--- scrape.js
+++ scrape.js
@@ -7,6 +7,8 @@ const puppeteer = require('puppeteer-extra')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin());
 
+
+
 (async () => {
   try {
     // Create a browser instance
@@ -15,18 +17,15 @@ puppeteer.use(StealthPlugin());
     // Create a new page
     const page = await browser.newPage();
 
-    const website_url =
-      "https://www.copart.com/vehicleFinderSearch?searchStr=%7B%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23DamageTypeCode:DAMAGECODE_FR%22,%22%23OdometerReading:%5B0%20TO%209999999%5D%22,%22%23LotYear:%5B2013%20TO%202024%5D%22%5D,%22sortByZip%22:false,%22buyerEnteredZip%22:null,%22milesAway%22:null%7D%20&displayStr=FRONT%20END,%5B0%20TO%209999999%5D,%5B2013%20TO%202024%5D&from=%2FvehicleFinder";
+    //Navigation items
+const website_url =
+  "https://www.copart.com/vehicleFinderSearch?searchStr=%7B%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23DamageTypeCode:DAMAGECODE_FR%22,%22%23OdometerReading:%5B0%20TO%209999999%5D%22,%22%23LotYear:%5B2013%20TO%202024%5D%22%5D,%22sortByZip%22:false,%22buyerEnteredZip%22:null,%22milesAway%22:null%7D%20&displayStr=FRONT%20END,%5B0%20TO%209999999%5D,%5B2013%20TO%202024%5D&from=%2FvehicleFinder";
 
-    let auction_hover =
-      '//*[@id="main-container"]/div/app-root/lot-search-results/search-results/div/div[2]/div[2]/search-table-component';
-
+let auction_hover =
+  '//*[@id="main-container"]/div/app-root/lot-search-results/search-results/div/div[2]/div[2]/search-table-component';
+    
     // Open URL in current page
     await page.goto(website_url);
-
-    await page.waitForSelector("img");
-
-    await page.waitForTimeout(2000);
 
     // Capture screenshot
     await page.screenshot({
@@ -35,18 +34,16 @@ puppeteer.use(StealthPlugin());
 
     console.log(`check the screenshotOnLoad. ✨`);
 
-    // Get the first h1 element using page.$x
-    // let first_h1_element = await page.$x(auction_tab);
+    await page.waitForSelector("img");
 
-    // Get the textContent of the h1 element
-    //let h1_value = await page.evaluate(el => el.textContent, first_h1_element[0])
+    await page.waitForTimeout(2000);    
 
     // Evaluate the XPath expression to get the matching element
     let elementHandle = await page.waitForXPath(auction_hover);
     console.log(elementHandle);
 
     let h1_value = await page.evaluate((el) => el.textContent, elementHandle);
-    console.log(h1_value);
+    //console.log(h1_value);
 
     // Use page.evaluate to extract the href attribute of the first <a> element
     let hrefValue = await page.evaluate((element) => {
@@ -56,12 +53,27 @@ puppeteer.use(StealthPlugin());
 
     console.log(hrefValue);
 
-    //await page.click(auction_selector)
-    //await page.waitForSelector('img')
+    //console.log(typeof(hrefValue + website_url))
+
+    if (hrefValue) {
+      // Use page.goto to navigate to the URL specified in the href attribute
+      await page.goto(website_url + hrefValue);
+      
+      // Now you are on the new page, and you can perform actions or extract data as needed.
+      await page.waitForSelector('img')// wait for an image to appear
+
+      await page.waitForTimeout(2000); 
+
+      await page.screenshot({
+        path: "screenshotOnCarPage.jpg",
+      });
+    }
+    
+
 
     // Capture screenshot
     await page.screenshot({
-      path: "screenshot.jpg",
+      path: "screenshotOnClose.jpg",
     });
 
     // Close the browser instance
