2dc9dab91360b97449bbe2ffaab9c3ac9d736eb5
broken99records <40528283+broken99records@users.noreply.github.com>
2023-09-06 15:04:04 -0400

    added code to evaluate the first href attribute

--- scrape.js
+++ scrape.js
@@ -10,70 +10,65 @@ puppeteer.use(StealthPlugin());
 (async () => {
   try {
     // Create a browser instance
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({ headless: true });
 
     // Create a new page
     const page = await browser.newPage();
 
-    
+    const website_url =
+      "https://www.copart.com/vehicleFinderSearch?searchStr=%7B%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23DamageTypeCode:DAMAGECODE_FR%22,%22%23OdometerReading:%5B0%20TO%209999999%5D%22,%22%23LotYear:%5B2013%20TO%202024%5D%22%5D,%22sortByZip%22:false,%22buyerEnteredZip%22:null,%22milesAway%22:null%7D%20&displayStr=FRONT%20END,%5B0%20TO%209999999%5D,%5B2013%20TO%202024%5D&from=%2FvehicleFinder";
 
-    const website_url = 'https://www.copart.com/vehicleFinderSearch?searchStr=%7B%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23DamageTypeCode:DAMAGECODE_FR%22,%22%23OdometerReading:%5B0%20TO%209999999%5D%22,%22%23LotYear:%5B2013%20TO%202024%5D%22%5D,%22sortByZip%22:false,%22buyerEnteredZip%22:null,%22milesAway%22:null%7D%20&displayStr=FRONT%20END,%5B0%20TO%209999999%5D,%5B2013%20TO%202024%5D&from=%2FvehicleFinder';
-    
-    let auction_hover = '//*[@id="main-container"]/div/app-root/lot-search-results/search-results/div/div[2]/div[2]/search-table-component'
-       
-    
+    let auction_hover =
+      '//*[@id="main-container"]/div/app-root/lot-search-results/search-results/div/div[2]/div[2]/search-table-component';
+
     // Open URL in current page
     await page.goto(website_url);
 
-    await page.waitForSelector('img')
+    await page.waitForSelector("img");
 
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(2000);
 
     // Capture screenshot
     await page.screenshot({
-      path: 'screenshotOnLoad.jpg',
+      path: "screenshotOnLoad.jpg",
     });
 
-    console.log(`check the screenshotOnLoad. ✨`)
+    console.log(`check the screenshotOnLoad. ✨`);
 
     // Get the first h1 element using page.$x
-   // let first_h1_element = await page.$x(auction_tab);
+    // let first_h1_element = await page.$x(auction_tab);
 
-  // Get the textContent of the h1 element
+    // Get the textContent of the h1 element
     //let h1_value = await page.evaluate(el => el.textContent, first_h1_element[0])
 
-    
-
-    
     // Evaluate the XPath expression to get the matching element
     let elementHandle = await page.waitForXPath(auction_hover);
-    console.log(elementHandle)
+    console.log(elementHandle);
 
-    let h1_value = await page.evaluate(el => el.textContent, elementHandle)
-    console.log(h1_value)
+    let h1_value = await page.evaluate((el) => el.textContent, elementHandle);
+    console.log(h1_value);
 
+    // Use page.evaluate to extract the href attribute of the first <a> element
+    let hrefValue = await page.evaluate((element) => {
+      const firstAnchor = element.querySelector("a"); // Find the first <a> element
+      return firstAnchor ? firstAnchor.getAttribute("href") : null; // Get the href attribute
+    }, elementHandle);
 
+    console.log(hrefValue);
 
     //await page.click(auction_selector)
     //await page.waitForSelector('img')
 
     // Capture screenshot
     await page.screenshot({
-      path: 'screenshot.jpg',
+      path: "screenshot.jpg",
     });
-
-
-    
-
-     
-      
 
     // Close the browser instance
     await browser.close();
 
     //console.log(h1_value)
-    console.log(`All done, check the screenshot. ✨`)
-   
+    console.log(`All done, check the screenshot. ✨`);
   } catch (error) {
 
     console.error(error);
